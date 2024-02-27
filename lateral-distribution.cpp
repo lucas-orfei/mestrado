5d437cae07fce5d317151e500a28431fcacada58
Lucas Orfei <106162822+lucas-orfei@users.noreply.github.com>
2024-02-27 11:52:38 -0300

    Add files via upload

--- lateral-distribution.cpp
+++ lateral-distribution.cpp
@@ -8,15 +8,15 @@ Char_t linha[10000];
 
     int number_of_lines = 0;
     std::string line;
-    std::ifstream myfile("iron/DAT4561514_tratado");
+    std::ifstream myfile("proton/DAT4011514_tratado");
 
     while (std::getline(myfile, line))
         ++number_of_lines;
-    std::cout << "Number of lines in text file: " << number_of_lines << "\n";
-    return 0;
+    std::cout << "Número de linhas: " << number_of_lines << "\n";
+    //return 0;
 
 
-FILE *fp1 = fopen("iron/DAT4561514_tratado","r");
+FILE *fp1 = fopen("proton/DAT4011514_tratado","r");
 
 Int_t nlines=0;
 Float_t part,x_p, y_p, r_p, w,d_r_p;
@@ -31,7 +31,7 @@ c1 = new TCanvas("c1","Histograma unidimensional",10,10,700,700);
 
 //c1->SetLogx(1);
 
-hpx= new TH1F("hpx","Primary iron 4.64E15 eV",100,0,1000);
+hpx= new TH1F("hpx","Primary proton 4.64E15 eV",100,0,1000);
 
 while (nlines<number_of_lines) {
 	int ch;
@@ -53,6 +53,10 @@ hpx->GetXaxis()->SetTitle("(cm)");
 //c1->SetLogx();
 hpx->Draw();
 
+rho_sum = hpx->Integral(0,100);
+
+cout << "Densidade de partículas: " << rho_sum << endl;
+
 // Lateral distribution function - Nishimura-Kamata-Greisen (NKG)
 //*TMath::Exp([4]*TMath::Log(1+0.35*(x/[3])))
 
@@ -63,7 +67,7 @@ TF1 *f = new TF1("fit", "([0]/([3]*[3]))*TMath::Exp(-[1]*TMath::Log(x/[3]))*TMat
 	//f->FixParameter(1,0.88);
 	//f->FixParameter(2,1);
 	f->SetParNames("C","a","b","r0");
-	//hpx->Fit("fit");
+	hpx->Fit("fit");
 
 // Creates a Root function
 /* TF1 *f = new TF1("fit","[0]*TMath::Exp(-[1]*TMath::Log(x))",1,300);
