511386d0118a7e4220958301983a2a03235f6b2d
Lucas Orfei <106162822+lucas-orfei@users.noreply.github.com>
2024-04-18 13:48:44 -0300

    Anéis de densidade
    Implementação dos anéis de densidade para visualização no canvas c2

--- lateral-distribution.cpp
+++ lateral-distribution.cpp
@@ -8,31 +8,42 @@ Char_t linha[10000];
 
     int number_of_lines = 0;
     std::string line;
-    std::ifstream myfile("iron/DAT4561514_tratado");
+    std::ifstream myfile("/home/orfei/mestrado/progs/lateral_distribution/data/photon/DAT4001514_tratado");
 
     while (std::getline(myfile, line))
         ++number_of_lines;
-    std::cout << "Número de linhas: " << number_of_lines << "\n";
+    std::cout << "Numero de linhas: " << number_of_lines << "\n";
     //return 0;
 
 
-FILE *fp1 = fopen("iron/DAT4561514_tratado","r");
+FILE *fp1 = fopen("/home/orfei/mestrado/progs/lateral_distribution/data/photon/DAT4001514_tratado","r");
 
-Int_t nlines=0;
-Float_t part,x_p, y_p, r_p, w,d_r_p;
+Int_t nlines=0,npart=0;
+Float_t part,x_p, y_p, r_p,w,d_r_p;
+
+Float_t xdet1=0,ydet1=346;
+Float_t xdet2=-400,ydet2=-346;
+Float_t xdet3=400,ydet3=-346;
+
+Float_t r1,r2,r3,rdet=65;
 
 //num_lin = 20976079;
 sqrt_lin = TMath::Sqrt(number_of_lines);
-d_r_p = 10000/sqrt_lin;
+d_r_p = 6.036;
 //r_0 = 78;
 
 
 c1 = new TCanvas("c1","Histograma unidimensional",10,10,700,700);
+c2 = new TCanvas("c2","Histograma bidimensional",10,10,700,700);
 
 //c1->SetLogx(1);
 
-hpx= new TH1F("hpx","Primary iron 4.64E15 eV",100,0,1000);
+hpx= new TH1F("hpx","Primary photon 4.64E15 eV",165,0,1000);
 
+//h2d= new TH2F("h2d","Primary proton 1E15 eV",1000,-500,500,1000,-500,500);
+
+
+//while (nlines<number_of_lines/100) {
 while (nlines<number_of_lines) {
 	int ch;
 	ch=fgetc(fp1);
@@ -45,24 +56,39 @@ while (nlines<number_of_lines) {
          //cout << r_p << endl;
          w = 1/(100*2*TMath::Pi()*r_p*d_r_p);
          //cout << TMath::Pi() << " " << r_p << " " << part << " "<< d_r_p << " " << w << endl;
-    hpx->Fill(r_p,w);
-    nlines++;
+    	hpx->Fill(r_p,w);
+    
+		r1=sqrt((x_p-xdet1)*(x_p-xdet1)+(y_p-ydet1)*(y_p-ydet1));
+		r2=sqrt((x_p-xdet2)*(x_p-xdet2)+(y_p-ydet2)*(y_p-ydet2));
+		r3=sqrt((x_p-xdet3)*(x_p-xdet3)+(y_p-ydet3)*(y_p-ydet3));
+
+//		if ((r_p>=281)&&(r_p<=411))
+/*		if ((r1<=rdet)||(r2<=rdet)||(r3<=rdet))
+			{
+			//cout << r_p << endl;
+			h2d->Fill(x_p,y_p,0.01);
+			npart++;
+			}
+    
+*/	nlines++;
    }
 c1->Update();
 hpx->GetXaxis()->SetTitle("(cm)");
 //c1->SetLogx();
 hpx->Draw();
 
-rho_sum = hpx->Integral(0,101);
+rho_sum = hpx->Integral(0,166);
 
-cout << "Densidade de partículas: " << rho_sum << endl;
+cout << "Densidade de particulas: " << rho_sum << endl;
 
 bin_346 = hpx->GetXaxis()->FindBin(346);
-cout << "Número do Bin em 346 cm: " << bin_346 << endl;
+cout << "Numero do bin em 346 cm: " << bin_346 << endl;
 
-rho_346 = hpx->Integral(35,35);
+rho_346 = hpx->Integral(58,58);
 
-cout << "Densidade de partículas em 346 cm: " << rho_346 << endl;
+cout << "Densidade de particulas em 346 cm: " << rho_346 << endl;
+
+//cout << "Numero medio de particulas em torno de 346 cm pelo h2: " << npart/300 << endl;
 
 // Lateral distribution function - Nishimura-Kamata-Greisen (NKG)
 //*TMath::Exp([4]*TMath::Log(1+0.35*(x/[3])))
@@ -84,6 +110,9 @@ TF1 *f = new TF1("fit", "([0]/([3]*[3]))*TMath::Exp(-[1]*TMath::Log(x/[3]))*TMat
 
  hpx->Fit("fit");*/
 
+
+//  c2->Update();
+//  h2d->Draw();
 }
 
 
