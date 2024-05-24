e3c0cb36b06412e1f640e3bc143c0d7edc363280
Lucas Orfei <106162822+lucas-orfei@users.noreply.github.com>
2024-05-24 10:41:23 -0300

    Integral para o N_e
    Integral até 5,29 m para calcular o número de elétrons.

--- lateral-distribution.cpp
+++ lateral-distribution.cpp
@@ -8,7 +8,7 @@ Char_t linha[10000];
 
     int number_of_lines = 0;
     std::string line;
-    std::ifstream myfile("/home/orfei/mestrado/progs/lateral_distribution/data/photon/DAT4001514_tratado");
+    std::ifstream myfile("/home/orfei/mestrado/progs/lateral_distribution/eletrons/data/iron/DAT4561514_tratado");
 
     while (std::getline(myfile, line))
         ++number_of_lines;
@@ -16,7 +16,7 @@ Char_t linha[10000];
     //return 0;
 
 
-FILE *fp1 = fopen("/home/orfei/mestrado/progs/lateral_distribution/data/photon/DAT4001514_tratado","r");
+FILE *fp1 = fopen("/home/orfei/mestrado/progs/lateral_distribution/eletrons/data/iron/DAT4561514_tratado","r");
 
 Int_t nlines=0,npart=0;
 Float_t part,x_p, y_p, r_p,w,d_r_p;
@@ -30,15 +30,15 @@ Float_t r1,r2,r3,rdet=65;
 //num_lin = 20976079;
 sqrt_lin = TMath::Sqrt(number_of_lines);
 d_r_p = 6.036;
-//r_0 = 78;
+r_0 = 78;
 
 
 c1 = new TCanvas("c1","Histograma unidimensional",10,10,700,700);
-c2 = new TCanvas("c2","Histograma bidimensional",10,10,700,700);
+//c2 = new TCanvas("c2","Histograma bidimensional",10,10,700,700);
 
 //c1->SetLogx(1);
 
-hpx= new TH1F("hpx","Primary photon 4.64E15 eV",165,0,1000);
+hpx= new TH1F("hpx","Primary iron 4.64E15 eV",165,0,1000);
 
 //h2d= new TH2F("h2d","Primary proton 1E15 eV",1000,-500,500,1000,-500,500);
 
@@ -81,6 +81,13 @@ rho_sum = hpx->Integral(0,166);
 
 cout << "Densidade de particulas: " << rho_sum << endl;
 
+bin_529 = hpx->GetXaxis()->FindBin(529);
+cout << "Numero do bin em 500 cm: " << bin_529 << endl;
+
+rho_529 = hpx->Integral(0,88);
+
+cout << "Número total de particulas até 529 cm: " << rho_529 << endl;
+
 bin_346 = hpx->GetXaxis()->FindBin(346);
 cout << "Numero do bin em 346 cm: " << bin_346 << endl;
 
@@ -91,15 +98,17 @@ cout << "Densidade de particulas em 346 cm: " << rho_346 << endl;
 //cout << "Numero medio de particulas em torno de 346 cm pelo h2: " << npart/300 << endl;
 
 // Lateral distribution function - Nishimura-Kamata-Greisen (NKG)
-//*TMath::Exp([4]*TMath::Log(1+0.35*(x/[3])))
 
-TF1 *f = new TF1("fit", "([0]/([3]*[3]))*TMath::Exp(-[1]*TMath::Log(x/[3]))*TMath::Exp(-[2]*TMath::Log(1+(x/[3])))",0,1000000);
-	
-	f->SetParameters(300,-10,12,1000);
+//TF1 *f = new TF1("fit", "([0]/([3]*[3]))*TMath::Exp(-[1]*TMath::Log(x/[3]))*TMath::Exp(-[2]*TMath::Log(1+(x/[3])))",0,1000000);
+
+
+TF1 *f = new TF1("fit", "([0]/([2]*[2]))*TMath::Exp(([1]-2)*TMath::Log(x/[2]))*TMath::Exp(([1]-4.5)*TMath::Log(1+(x/[2])))",0,1000000);	
+
+	f->SetParameters(300,0.5,7800);
 	//f->FixParameter(0,0.0006);
 	//f->FixParameter(1,0.88);
-	//f->FixParameter(2,1);
-	f->SetParNames("C","a","b","r0");
+	f->FixParameter(2,7800);
+	f->SetParNames("C","s","r0");
 	hpx->Fit("fit");
 
 // Creates a Root function
